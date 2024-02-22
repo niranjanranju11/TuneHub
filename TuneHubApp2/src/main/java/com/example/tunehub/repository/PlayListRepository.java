d116cd15d80892c0a0c737b510ec9fe843d76764
niranjanranju11 <ranjugandhi111@gmail.com>
2024-02-22 11:01:39 +0530

    Initial commit

--- TuneHubApp2/src/main/java/com/example/tunehub/repository/PlayListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tunehub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tunehub.entities.PlayList;
-
-public interface PlayListRepository extends JpaRepository<PlayList,Integer> {
-
-}
